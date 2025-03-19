a2f05130b7e2fa114f1cf2c927ff55b92e1206f6
violetann894 <rachelhuss2976@gmail.com>
2025-03-19 15:13:06 -0400

    initial commit

---
+++ StatsLibrary/src/PoissonDistribution.java
@@ -0,0 +1,80 @@
+import java.math.BigDecimal;
+import java.math.BigInteger;
+
+/**
+ *
+ */
+public class PoissonDistribution {
+
+    /**
+     *
+     * @param n
+     * @param k
+     * @param scalar
+     * @param y
+     * @return
+     */
+    public double poissonDistribution(int n, int k, int scalar, int y){
+
+    }
+
+    /**
+     *
+     * @param n
+     * @param k
+     * @param scalar
+     * @param y
+     * @return
+     */
+    public BigDecimal poissonDistribution(BigInteger n, BigInteger k, BigInteger scalar, BigInteger y){
+
+    }
+
+    /**
+     *
+     * @param n
+     * @param k
+     * @param scalar
+     * @return
+     */
+    public double lambda(int n, int k, int scalar){
+
+    }
+
+    /**
+     *
+     * @param n
+     * @param k
+     * @param scalar
+     * @return
+     */
+    public BigDecimal lambda(BigInteger n, BigInteger k, BigInteger scalar){
+
+    }
+
+    /**
+     *
+     * @param lambda
+     * @return
+     */
+    public double standardDeviation(double lambda){
+
+    }
+
+    /**
+     *
+     * @param lambda
+     * @return
+     */
+    public BigDecimal standardDeviation(BigDecimal lambda){
+
+    }
+
+    /**
+     *
+     */
+    public void tester(){
+
+    }
+
+}
