e9bdb9bdb4fbc85f3c51121627a6ba60ae711da8
violetann894 <rachelhuss2976@gmail.com>
2025-03-20 15:42:06 -0400

    Added comments and functionality

--- FunctionCSV/src/Exporter.java
+++ FunctionCSV/src/Exporter.java
@@ -1,75 +1,45 @@
-import javax.swing.*;
-import java.io.File;
 import java.io.FileNotFoundException;
 import java.io.PrintWriter;
 import java.util.ArrayList;
-import java.util.NoSuchElementException;
-import java.util.Scanner;
 
+/**
+ * The Exporter class holds a method that handles placing x and y data into a csv file.
+ * @author Rachel Hussmann
+ */
 public class Exporter {
-    public static void main(String[] args) {
-        JFileChooser picker = new JFileChooser();
-        ArrayList<String> fileContents = new ArrayList<>();
 
-        if(picker.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
-            System.out.println("No File was picked . . . Exiting");
-            System.exit(0);
-        }
+    /**
+     * The createFile method creates a csv file called "Function.csv" that holds the X and Y values of a mathematical
+     * function.
+     * @param xValues The ArrayList of x values that were used to with the function to calculate the y values
+     * @param yValues The ArrayList of y values that were calculated using the function
+     */
+    public void createFile(ArrayList<Integer> xValues, ArrayList<Double> yValues){
 
-        // Open the file
-        File file = new File(picker.getSelectedFile().toString());
-        System.out.println("Opening: " + picker.getSelectedFile().toString());
-
-        // Create the Scanner object and scope to the entire main method
-        Scanner in = null;
-
-        try {
-            // Try to open the file
-            in = new Scanner(file);
-
-            try {
-                // Read in the data one line at a time
-                while(in.hasNextLine()) {
-                    // Do something with this String
-                    fileContents.add(in.nextLine());
-                }
-
-
-                // Fix anything that broke and clean up
-            } catch (NoSuchElementException e) {
-                System.err.println("Record Error: " + e.getMessage());
-            } catch (IndexOutOfBoundsException e) {
-                System.err.println("Parse Error: " + e.getMessage());
-            } catch (NumberFormatException e) {
-                System.err.println("Data Error: " + e.getMessage());
-            } finally {
-                in.close();
-            }
-
-        } catch (FileNotFoundException e) {
-            System.err.println("File Unavailable: " + e.getMessage());
-        }
-
+        //Using a try catch block due to chance of an exception being thrown
         try {
 
-            if(picker.showOpenDialog(null) != JFileChooser.APPROVE_OPTION) {
-                System.out.println("No File was picked . . . Exiting");
-                System.exit(0);
+            //Create a PrintWriter object to handle copying the data into a file
+            PrintWriter out = new PrintWriter("Function.csv");
+
+            //Create the header for the csv file so it knows how to categorize the data
+            out.println("X,Y,");
+
+            //Iterate through the xValues and yValues list to format the data and print it to the csv file
+            for(int i = 0; i < xValues.size(); i++){
+                out.printf("%d,%.4f,\n", xValues.get(i), yValues.get(i));
             }
 
-            // Try to open the file
-            PrintWriter out = new PrintWriter(new File(picker.getSelectedFile().toString()));
-            System.out.println("Opening: " + picker.getSelectedFile().toString());
+            //Inform the user if the creation of the file was successful
+            System.out.println("File Creation Successful");
 
-            for(String line : fileContents) {
-                System.out.println(line);
-                out.println(line);
-            }
-
+            //Close the PrintWriter object
             out.close();
 
         } catch (FileNotFoundException e) {
-            System.err.println("File Access Error: " + e.getMessage());
+
+            //If a FileNotFoundException is thrown, catch it and inform the user of the error
+            System.err.println("File Could Not Be Created: " + e.getMessage());
         }
     }
 }
--- FunctionCSV/src/Function.java
+++ FunctionCSV/src/Function.java
@@ -1,2 +1,44 @@
+import java.util.ArrayList;
+
+/**
+ * The Function class calculates the answer to a mathematical function using x and y values.
+ * @author Rachel Hussmann
+ */
 public class Function {
+
+    /**
+     * The logFunction method calculates the value of ln(x) and save the x and y values into ArrayLists for saving.
+     * @param startingValue The starting x value (inclusive)
+     * @param finishingValue The last x value (inclusive)
+     */
+    public void logFunction(int startingValue, int finishingValue){
+
+        //Create the ArrayLists that will hold the x and y values
+        ArrayList<Integer> xValues = new ArrayList<>();
+        ArrayList<Double> yValues = new ArrayList<>();
+
+        //Loop through the x values and save the x and y values
+        for(int i = startingValue; i <= finishingValue; i++){
+            xValues.add(i);
+            yValues.add(Math.log(i));
+        }
+
+        //Call the saveFunction method to save the x and y values in a csv file
+        saveFunction(xValues, yValues);
+    }
+
+    /**
+     * The saveFunction method creates an Exporter object and saves the x and y values from the function that was
+     * calculated.
+     * @param xValues The ArrayList of x values from the function
+     * @param yValues The ArrayList of y values calculated using the function and the x values
+     */
+    public void saveFunction(ArrayList<Integer> xValues, ArrayList<Double> yValues){
+
+        //Create the exporter object
+        Exporter exporter = new Exporter();
+
+        //Call the createFile method to save the values in a csv file
+        exporter.createFile(xValues, yValues);
+    }
 }
