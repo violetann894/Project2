79495eda95487f2bb6a0d50967d76600961f9865
violetann894 <rachelhuss2976@gmail.com>
2025-03-20 15:11:45 -0400

    Initial Commit

---
+++ FunctionCSV/src/FunctionCSVTester.java
@@ -0,0 +1,16 @@
+/**
+ * The FunctionCSVTester is the tester class for the Exporter and Function classes. It contains the main method to
+ * run the methods called.
+ * @author Rachel Hussmann
+ */
+public class FunctionCSVTester {
+
+    public static void main(String[] args) {
+
+        //Create the function object
+        Function function = new Function();
+
+        //Call the logFunction to calculate and save log values
+        function.logFunction(1, 1000000);
+    }
+}
