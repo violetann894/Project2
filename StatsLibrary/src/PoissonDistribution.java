b038974fa2f7d019b9567438f23ddcdb6be8cae5
violetann894 <rachelhuss2976@gmail.com>
2025-03-19 21:29:48 -0400

    Added functionality

--- StatsLibrary/src/PoissonDistribution.java
+++ StatsLibrary/src/PoissonDistribution.java
@@ -1,8 +1,12 @@
 import java.math.BigDecimal;
 import java.math.BigInteger;
+import java.math.MathContext;
+import java.math.RoundingMode;
 
 /**
- *
+ * The PoissonDistribution class contains methods that calculate the probability of en event based on a
+ * Poisson Probability Distribution.
+ * @author Rachel Hussmann
  */
 public class PoissonDistribution {
 
@@ -31,43 +35,48 @@ public class PoissonDistribution {
     }
 
     /**
-     *
-     * @param n
-     * @param k
-     * @param scalar
-     * @return
+     * The lambda method finds the value of lambda for a Poisson distribution. Lambda is equal to the mean and variance
+     * of the distribution, so this method can be used to find either.
+     * @param n The number of units
+     * @param k The Total number of events
+     * @param scalar Scalar value for lambda
+     * @return The value of lambda
      */
     public double lambda(int n, int k, int scalar){
-
+        return (double) (k/n)*scalar;
     }
 
     /**
-     *
-     * @param n
-     * @param k
-     * @param scalar
-     * @return
+     * The lambda method finds the value of lambda for a Poisson distribution. Lambda is equal to the mean and variance
+     * of the distribution, so this method can be used to find either.
+     * @param n The number of units
+     * @param k The Total number of events
+     * @param scalar Scalar value for lambda
+     * @return The value of lambda
      */
     public BigDecimal lambda(BigInteger n, BigInteger k, BigInteger scalar){
-
+        BigDecimal nDecimal = new BigDecimal(n);
+        BigDecimal kDecimal = new BigDecimal(k);
+        BigDecimal scalarDecimal = new BigDecimal(scalar);
+        return kDecimal.divide(nDecimal, 3, RoundingMode.UNNECESSARY).multiply(scalarDecimal);
     }
 
     /**
-     *
-     * @param lambda
-     * @return
+     * The standardDeviation method finds the standard deviation of a Poisson Distribution.
+     * @param lambda The mean or variance of the distribution
+     * @return The standard deviation of the distribution
      */
     public double standardDeviation(double lambda){
-
+        return Math.sqrt(lambda);
     }
 
     /**
-     *
-     * @param lambda
-     * @return
+     * The standardDeviation method finds the standard deviation of a Poisson Distribution.
+     * @param lambda The mean or variance of the distribution
+     * @return The standard deviation of the distribution
      */
     public BigDecimal standardDeviation(BigDecimal lambda){
-
+        return lambda.sqrt(MathContext.DECIMAL32);
     }
 
     /**
