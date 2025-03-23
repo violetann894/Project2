f6d123239b9176a479a7ed6e5437a0c60d52165b
violetann894 <rachelhuss2976@gmail.com>
2025-03-22 21:57:43 -0400

    Added functionality and comments

--- FunctionCSV/src/FunctionCSVTester.java
+++ FunctionCSV/src/FunctionCSVTester.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 /**
  * The FunctionCSVTester is the tester class for the Exporter and Function classes. It contains the main method to
  * run the methods called.
@@ -10,7 +12,20 @@ public class FunctionCSVTester {
         //Create the function object
         Function function = new Function();
 
-        //Call the logFunction to calculate and save log values
+        //Call the logFunction method to calculate and save log values
         function.logFunction(1, 1000, -500, 501);
+
+        //Create the importer object
+        Importer importer = new Importer();
+
+        //Import the SaltedLogFunction.csv to smooth it
+        ArrayList<Double> importedData =
+                importer.importFile("C:\\Users\\Rache\\IdeaProjects\\Project2\\SaltedLogFunction.csv");
+
+        //Create the smoother object
+        Smoother smoother = new Smoother();
+
+        //Call the smoothData method to save the smoothed data
+        smoother.smoothData(importedData, 5);
     }
 }
