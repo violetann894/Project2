4cb06552f065c14b78b81c2319c4e59db9465e8e
violetann894 <rachelhuss2976@gmail.com>
2025-03-30 21:02:46 -0400

    Added comments

--- FunctionCSV/src/Smoother.java
+++ FunctionCSV/src/Smoother.java
@@ -10,8 +10,8 @@ public class Smoother {
     private ArrayList<Double> yValues;
 
     /**
-     *
-     * @param yValues
+     * Additional constructor for the Smoother class
+     * @param yValues The salted y values from the function
      */
     public Smoother(ArrayList<Double> yValues){
         this.xValues = new ArrayList<>();
@@ -28,31 +28,47 @@ public class Smoother {
         ArrayList<Integer> tempXValues = new ArrayList<>();
         ArrayList<Double> tempYValues = new ArrayList<>();
 
+        //Initialize the xCount value to keep track of the x values
         int xCount = 1;
 
+        //Outer for loop iterates through the salted dataset
         for(int i = 0; i < yValues.size(); i++){
+
+            //Initialize the sum and average count needed to create the averaged values
             double sum = 0.0;
             int averageCount = 0;
 
+            //First for loop adds up the left side values and the current i value
             for(int j = 1; j <= windowValue; j++){
+
+                //Check to see if the value is outside the range of the list
                 if(i-j >= 0){
+
+                    //If the value is within the bounds of the list, add it to the sum and increment the average count
                     sum += yValues.get(i-j);
                     averageCount++;
                 }
             }
 
+            //Second for loop adds up the right side values
             for(int j = 1; j <= windowValue; j++){
+
+                //Check to see if the value is outside the range of the list
                 if(i+j < yValues.size()){
+
+                    //
                     sum += yValues.get(i+j);
                     averageCount++;
                 }
             }
 
+            //If the value is within the bounds of the list, add it to the sum and increment the average count
             tempXValues.add(xCount);
             xCount++;
             tempYValues.add(sum/averageCount);
         }
 
+        //clear out the xValues and yValues arraylists to copy the new values into them
         xValues.clear();
         xValues.addAll(tempXValues);
         yValues.clear();
@@ -71,15 +87,21 @@ public class Smoother {
     }
 
     /**
-     *
-     * @param windowValue
-     * @param numberOfRuns
+     * The runSmoother method is responsible for running the smoothData method the number of times the user would like
+     * and saving the results of the smoothing.
+     * @param windowValue The number of values to left and to the right of each value to average
+     * @param numberOfRuns The number of times to run the smoothData method
      */
     public void runSmoother(int windowValue, int numberOfRuns){
+
+        //Runs the smoothData method the user requested number of times
         for(int i = 0; i < numberOfRuns; i++){
+
+            //Call the smoothData method
             smoothData(windowValue);
         }
 
+        //Save the data that has been smoothed
         saveSmoothedFunction();
     }
 }
