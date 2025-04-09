7f2f387384cb63b744a35313987bda220af8d43d
violetann894 <rachelhuss2976@gmail.com>
2025-04-08 22:52:21 -0400

    Added methods and comments

--- PSSPart3/src/main/java/Function.java
+++ PSSPart3/src/main/java/Function.java
@@ -1,5 +1,6 @@
+import org.jfree.data.xy.XYDataset;
+
 import java.util.ArrayList;
-import org.jfree.chart.*;
 
 /**
  * The Function class holds a method that calculates the answer to a mathematical function using x and y values and a
@@ -8,18 +9,23 @@ import org.jfree.chart.*;
  */
 public class Function {
 
+    private ArrayList<Integer> xValues;
+    private ArrayList<Double> yValues;
+
+    /**
+     * Default Constructor for the Function class
+     */
+    public Function(){
+        xValues = new ArrayList<>();
+        yValues = new ArrayList<>();
+    }
+
     /**
      * The logFunction method calculates the value of ln(x) and saves the x and y values into ArrayLists.
      * @param startingValue The starting x value (inclusive)
      * @param finishingValue The last x value (inclusive)
-     * @param origin The origin of the random function (inclusive)
-     * @param bound The upper bound of the random function (exclusive)
      */
-    public void logFunction(int startingValue, int finishingValue, int origin, int bound){
-
-        //Create the ArrayLists that will hold the x and y values
-        ArrayList<Integer> xValues = new ArrayList<>();
-        ArrayList<Double> yValues = new ArrayList<>();
+    public void logFunction(int startingValue, int finishingValue){
 
         //Loop through the x values and save the x and y values
         for(int i = startingValue; i <= finishingValue; i++){
@@ -27,8 +33,29 @@ public class Function {
             yValues.add(Math.log(i));
         }
 
-        //Call the saveFunction and saveSaltedFunction method to save the x and y values in a csv file
+        //Create a Plotter object
+        Plotter plotter = new Plotter();
 
+        //Create the dataset and plot it
+        XYDataset logData = plotter.createXYDataset(xValues, yValues, "Natural Log Function");
+
+        //Plot the XY graph for the data
+        plotter.plotXYData(logData, "Natural Log Function", 800, 800);
     }
 
+    /**
+     * Getter for the xValues variable
+     * @return The ArrayList of x values from the function
+     */
+    public ArrayList<Integer> getXValues(){
+        return xValues;
+    }
+
+    /**
+     * Getter for the yValues variable
+     * @return The ArrayList of y values from the function
+     */
+    public ArrayList<Double> getYValues(){
+        return yValues;
+    }
 }
