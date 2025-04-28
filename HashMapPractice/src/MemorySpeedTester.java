63a77d44790c7359eb7ce9c2b1017487f88bebe7
violetann894 <rachelhuss2976@gmail.com>
2025-04-28 17:45:33 -0400

    Initial Commit

---
+++ HashMapPractice/src/MemorySpeedTester.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+
+/**
+ * The MemorySpeedTester is the class responsible for experimenting with RachelSimpleHashMap class by importing .csv
+ * data of differing sizes and exporting a .txt file containing the number of collisions, the memory used and how long
+ * the program took.
+ * @author Rachel Hussmann
+ */
+public class MemorySpeedTester {
+    public static void main(String[] args) {
+        //Using HashMap with .csv data
+        System.out.println("Creating new hashmap . . . \n");
+        RachelSimpleHashMap<EmergencyCallEntry> entryHashMap = new RachelSimpleHashMap<>();
+        Importer importer = new Importer();
+        ArrayList<EmergencyCallEntry> entries =
+                importer.importFile("\\C:\\Users\\Rache\\IdeaProjects\\Project2\\HashMapPractice\\911Data.csv");
+        for(EmergencyCallEntry entry : entries){
+            entryHashMap.add(entry.getTwp(), entry);
+        }
+        Exporter exporter = new Exporter();
+        StopWatch stopwatch = new StopWatch();
+
+        stopwatch.start();
+        String data = entryHashMap.checkNumberOfCollisions();
+        stopwatch.stop();
+
+        Runtime runtime = Runtime.getRuntime();
+        double memoryUsed = (runtime.totalMemory() - runtime.freeMemory())/1048576.00;
+        String stringMemoryUsed = "\nAmount of memory used: " + memoryUsed + " MB";
+
+        String timeElapsed = "\nAmount of time used: " + stopwatch.getElapsedTime() + " seconds";
+
+        exporter.createFile("NumberOfCollisions.txt", data + stringMemoryUsed + timeElapsed);
+    }
+}
